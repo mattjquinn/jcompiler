8c6fdbb46d649c779bc9110e44fea84d0c3f112e
Matt Quinn <matt@mattjquinn.com>
2019-02-25 20:27:39 -0500

    Removing peeophole optimizations.

--- src/bounds.rs
+++ src/bounds.rs
@@ -3,8 +3,6 @@
 //! Calculate the maximum cell accessed by a BF program.
 
 #[cfg(test)]
-use std::collections::HashMap;
-#[cfg(test)]
 use std::num::Wrapping;
 
 use std::cmp::{max, Ord, Ordering};
@@ -106,22 +104,10 @@ fn movement(instr: &AstNode) -> (SaturatingInt, SaturatingInt) {
                 )
             }
         }
-        Increment { offset, .. } | Set { offset, .. } => (
+        Increment { offset, .. } => (
             SaturatingInt::Number(offset as i64),
             SaturatingInt::Number(0),
         ),
-        MultiplyMove { ref changes, .. } => {
-            let mut highest_affected = 0;
-            for cell in changes.keys() {
-                if *cell > highest_affected {
-                    highest_affected = *cell;
-                }
-            }
-            (
-                SaturatingInt::Number(highest_affected as i64),
-                SaturatingInt::Number(0),
-            )
-        }
         Loop { ref body, .. } => {
             let (max_in_body, net_in_body) = overall_movement(body);
 
@@ -181,68 +167,6 @@ fn multiple_ptr_increment_bounds() {
 }
 
 #[test]
-fn multiply_move_bounds() {
-    let mut dest_cells = HashMap::new();
-    dest_cells.insert(1, Wrapping(3));
-    dest_cells.insert(4, Wrapping(1));
-    let instrs = vec![
-        MultiplyMove {
-            changes: dest_cells,
-            position: Some(Position { start: 0, end: 0 }),
-        },
-        // Multiply move should have increased the highest cell
-        // reached, but not the current cell. This instruction
-        // should not affect the output:
-        PointerIncrement {
-            amount: 2,
-            position: Some(Position { start: 1, end: 1 }),
-        },
-    ];
-
-    assert_eq!(highest_cell_index(&instrs), 4);
-}
-
-/// Multiply move uses offsets to the current pointer value.
-/// Verify we add to the current pointer value.
-#[test]
-fn multiply_move_bounds_are_relative() {
-    let mut dest_cells = HashMap::new();
-    dest_cells.insert(1, Wrapping(5));
-    let instrs = vec![
-        // Move to cell #2.
-        PointerIncrement {
-            amount: 2,
-            position: Some(Position { start: 0, end: 0 }),
-        },
-        // Move (with multiply) to cell #3 (#2 offset 1).
-        MultiplyMove {
-            changes: dest_cells,
-            position: Some(Position { start: 0, end: 0 }),
-        },
-    ];
-
-    assert_eq!(highest_cell_index(&instrs), 3);
-}
-
-#[test]
-fn multiply_move_backwards_bounds() {
-    let mut dest_cells = HashMap::new();
-    dest_cells.insert(-1, Wrapping(2));
-    let instrs = vec![
-        PointerIncrement {
-            amount: 1,
-            position: Some(Position { start: 0, end: 0 }),
-        },
-        MultiplyMove {
-            changes: dest_cells,
-            position: Some(Position { start: 0, end: 0 }),
-        },
-    ];
-
-    assert_eq!(highest_cell_index(&instrs), 1);
-}
-
-#[test]
 fn unbounded_movement() {
     let instrs = parse("[>]").unwrap();
     assert_eq!(highest_cell_index(&instrs), MAX_CELL_INDEX);
@@ -284,21 +208,4 @@ fn increment_offset_bounds() {
         position: Some(Position { start: 0, end: 0 }),
     }];
     assert_eq!(highest_cell_index(&instrs), 5);
-}
-
-#[test]
-fn set_offset_bounds() {
-    let instrs = [
-        Set {
-            amount: Wrapping(2),
-            offset: 10,
-            position: Some(Position { start: 0, end: 0 }),
-        },
-        Set {
-            amount: Wrapping(2),
-            offset: 11,
-            position: Some(Position { start: 0, end: 0 }),
-        },
-    ];
-    assert_eq!(highest_cell_index(&instrs), 11);
 }
