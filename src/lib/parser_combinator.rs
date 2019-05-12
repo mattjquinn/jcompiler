700979d62eabeb4d309eac8b00cfb5169e88fe9c
Matt Quinn <matt@mattjquinn.com>
2019-05-11 21:02:59 -0700

    Adding more combinators.

--- src/lib/parser_combinator.rs
+++ src/lib/parser_combinator.rs
@@ -1,12 +1,44 @@
 #[derive(PartialEq, Debug, Clone)]
 pub enum AstNode {
     Integer(i32),
+    Add{lhs:Box<AstNode>, rhs:Box<AstNode>},
 }
 
 type ParseResult<'a, Output> = Result<(&'a str, Output), &'a str>;
 
 trait Parser<'a, Output> {
     fn parse(&self, input: &'a str) -> ParseResult<'a, Output>;
+
+    fn map<F, NewOutput>(self, map_fn: F) -> BoxedParser<'a, NewOutput>
+        where
+            Self: Sized + 'a,
+            Output: 'a,
+            NewOutput: 'a,
+            F: Fn(Output) -> NewOutput + 'a,
+    {
+        BoxedParser::new(map(self, map_fn))
+    }
+
+    fn pred<F>(self, pred_fn: F) -> BoxedParser<'a, Output>
+        where
+            Self: Sized + 'a,
+            Output: 'a,
+            F: Fn(&Output) -> bool + 'a,
+    {
+        BoxedParser::new(pred(self, pred_fn))
+    }
+
+    fn and_then<F, NextParser, NewOutput>(self, f: F)
+                                          -> BoxedParser<'a, NewOutput>
+        where
+            Self: Sized + 'a,
+            Output: 'a,
+            NewOutput: 'a,
+            NextParser: Parser<'a, NewOutput> + 'a,
+            F: Fn(Output) -> NextParser + 'a,
+    {
+        BoxedParser::new(and_then(self, f))
+    }
 }
 
 impl<'a, F, Output> Parser<'a, Output> for F
@@ -32,6 +64,30 @@ impl<'a, Output> BoxedParser<'a, Output> {
     }
 }
 
+fn and_then<'a, P, F, A, B, NextP>(parser: P, f: F)
+                                   -> impl Parser<'a, B>
+    where
+        P: Parser<'a, A>,
+        NextP: Parser<'a, B>,
+        F: Fn(A) -> NextP,
+{
+    move |input| match parser.parse(input) {
+        Ok((next_input, result)) => f(result).parse(next_input),
+        Err(err) => Err(err),
+    }
+}
+
+fn either<'a, P1, P2, A>(parser1: P1, parser2: P2)
+                         -> impl Parser<'a, A>
+    where
+        P1: Parser<'a, A>,
+        P2: Parser<'a, A>,
+{
+    move |input| match parser1.parse(input) {
+        ok @ Ok(_) => ok,
+        Err(_) => parser2.parse(input),
+    }
+}
 
 fn any_char(input: &str) -> ParseResult<char> {
     match input.chars().next() {
@@ -81,11 +137,12 @@ fn space0<'a>() -> impl Parser<'a, Vec<char>> {
     zero_or_more(whitespace_char())
 }
 
-fn whitespace_wrap<'a, P, A>(parser: P)
-                             -> impl Parser<'a, A>
+fn ws<'a, P, A>(parser: P)
+                -> impl Parser<'a, A>
     where
         P: Parser<'a, A>,
 {
+    // "whitespace wrap": parses whitespace, selects thing inside it.
     right(space0(), left(parser, space0()))
 }
 
@@ -144,8 +201,22 @@ fn number(input : &str) -> Result<(&str, AstNode), &str> {
     }
 }
 
+fn dyadic_expr(input: &str) -> Result<(&str, AstNode), &str> {
+    ws(number).and_then(|n| {
+        right(
+            ws(any_char.pred(|c| *c == '+')),
+            ws(number)
+        ).map(move |m| {
+            AstNode::Add{lhs: Box::new(m), rhs: Box::new(m)}
+        })
+    })
+}
+
 fn expr<'a>(input: &str) -> impl Parser<'a, AstNode> {
-    whitespace_wrap(number)
+    either(
+        ws(dyadic_expr),
+        ws(number)
+    )
 }
 
 pub fn parse(source: &str) -> Result<Vec<AstNode>, String> {
@@ -168,7 +239,7 @@ fn parse_single_number() {
 
 #[test]
 fn parse_whitespace_number() {
-   let parser = whitespace_wrap(number);
+   let parser = ws(number);
     assert_eq!(parser.parse(" 1 "),
                Ok(("", AstNode::Integer(1))));
     assert_eq!(parser.parse("      2 "),
