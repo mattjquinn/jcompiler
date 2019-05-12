e78cec0ecc05d13611765a8a0a279a0c7532b8ec
Travis CI User <travis@example.org>
2019-05-12 00:03:16 +0000

    Automatic Travis documentation build

--- master/jcompilerlib/sidebar-items.js
+++ master/jcompilerlib/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["compile",""],["print_usage",""]],"mod":[["compiler",""],["parser",""],["shell","This module defines a convenient API for shelling out to commands, handling stderr when they fail."]]});
+initSidebarItems({"fn":[["compile",""],["print_usage",""]],"mod":[["compiler",""],["parser",""],["parser_combinator",""],["shell","This module defines a convenient API for shelling out to commands, handling stderr when they fail."]]});
