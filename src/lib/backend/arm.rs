fec7caadc117a4a7a0aea1ab09d5842e6e8e9177
Matt Quinn <matt@mattjquinn.com>
2019-08-22 18:56:31 -0700

    Begin storing expressions in memory; all tests passing, with several shortcuts and hacks.

--- src/lib/backend/arm.rs
+++ src/lib/backend/arm.rs
@@ -6,17 +6,30 @@ use std::fs::File;
 
 use tempfile::NamedTempFile;
 use parser::AstNode;
-use backend::arm::CompilationUnit::{CompilerInstructions, CompilerInstruction};
+use std::ops::Add;
 
 pub struct ARMBackend {}
 
 #[derive(Debug)]
-enum CompilationUnit {
-    Integer(Vec<String>),
-    Terms(Vec<CompilationUnit>),
+struct ValueRef {
+    offset: i32,
+}
 
-    CompilerInstruction(String),
-    CompilerInstructions(Vec<String>)
+#[derive(Debug)]
+struct BasicBlock {
+    base_offset: i32,
+    instructions: Vec<String>,
+    value_refs: Vec<ValueRef>,
+}
+
+impl BasicBlock {
+    fn new() -> BasicBlock {
+        BasicBlock {
+            base_offset: 0,
+            instructions : Vec::new(),
+            value_refs : Vec::new()
+        }
+    }
 }
 
 impl ::Backend for ARMBackend {
@@ -37,68 +50,78 @@ impl ::Backend for ARMBackend {
             .expect("Intermediate file in which to write assembly");
         println!("Writing assembly file to {}", &assembly_filename);
 
-        let mut units = vec![
-            CompilerInstructions(vec![
-                ".data".to_string(),
-                "intfmt: .asciz \"%d\"".to_string(),
-                "nlfmt:  .asciz \"\\n\"".to_string(),
-                "spacefmt:  .asciz \" \"".to_string(),
-                ".text".to_string(),
-                ".global main".to_string(),
-                ".extern printf".to_string(),
-                "main:".to_string(),
-                "push {ip, lr}".to_string(),
-            ])
-        ];
+        let mut basic_blocks = Vec::new();
 
         for astnode in ast {
             match astnode {
                 parser::AstNode::Print(expr) => {
-                    match compile_expr(expr) {
-                        CompilationUnit::Terms(terms_units) => {
-                            let mut i : usize = 1;
-                            let len = terms_units.len();
-                            for term_unit in terms_units {
-                                units.push(term_unit);
+                    let mut basic_block = BasicBlock::new();
+                    compile_expr(&mut basic_block, expr);
 
-                                // Multiple printed terms are separated by space,
-                                // except for the last item
-                                if i < len {
-                                    let mut instrs = Vec::new();
-                                    instrs.push("ldr r0, =spacefmt".to_string());
-                                    instrs.push("bl printf".to_string());
-                                    units.push(CompilerInstructions(instrs))
-                                }
-                                i += 1;
-                            }
-                        },
-                        unit => units.push(unit)
+                    for value_ref in basic_block.value_refs.iter().rev() {
+                        basic_block.instructions.push("ldr r0, =intfmt".to_string());
+                        basic_block.instructions.push(format!("ldrb r1, [sp, # {}]", value_ref.offset));
+                        basic_block.instructions.push("bl printf".to_string());
+
+                        // Multiple printed terms are separated by space, except for the last item
+                        // TODO: This is a terrible way to check if we have iterated
+                        // to the "final" value ref in the list
+                        if value_ref.offset != 0 {
+                            basic_block.instructions.push("ldr r0, =spacefmt".to_string());
+                            basic_block.instructions.push("bl printf".to_string());
+                        }
                     }
 
                     // All printed expressions are terminated with a newline.
-                    let mut instrs = Vec::new();
-                    instrs.push("ldr r0, =nlfmt".to_string());
-                    instrs.push("bl printf".to_string());
-                    units.push(CompilerInstructions(instrs))
+                    basic_block.instructions.push("ldr r0, =nlfmt".to_string());
+                    basic_block.instructions.push("bl printf".to_string());
+
+                    // TODO: This needs to be done within a "cleanup" method of BasicBlock
+                    basic_block.instructions.push(
+                        format!("add sp, sp, # {}",
+                                basic_block.value_refs.last()
+                                    // TODO: Obviously this +4 needs to go (be more clear)
+                                    .map_or(0,|v| v.offset + 4)));
+
+                    basic_blocks.push(basic_block);
                 },
                 _ => panic!("Not ready to compile top-level AST node: {:?}", astnode)
             }
         }
 
-        units.push(CompilerInstruction("pop {ip, pc}".to_string()));
-
-        let mut instrs = Vec::new();
-        compilation_units_to_instrs(&mut instrs, units);
-
-        println!("Printing ARM...");
-        for instr in &instrs {
-            println!("{}", instr);
+        println!("Printing ARM basic blocks...");
+        for basic_block in &basic_blocks {
+            println!("{:?}", basic_block);
         }
 
+        // TODO: Move boilerplate writing of preamble/postamble elsewhere.
         println!("Writing ARM...");
-        for instr in &instrs {
+        let preamble = vec![
+            ".arch armv7-a".to_string(),
+            ".data".to_string(),
+            "intfmt: .asciz \"%d\"".to_string(),
+            "nlfmt:  .asciz \"\\n\"".to_string(),
+            "spacefmt:  .asciz \" \"".to_string(),
+            ".text".to_string(),
+            ".global main".to_string(),
+            ".extern printf".to_string(),
+            ".syntax unified".to_string(),
+            "main:".to_string(),
+            "push {ip, lr}".to_string(),
+        ];
+        for instr in preamble {
             writeln!(&assembly_file, "{}", instr).expect("write failure");
         }
+        for basic_block in &basic_blocks {
+            for instr in &basic_block.instructions {
+                writeln!(&assembly_file, "{}", instr).expect("write failure");
+            }
+        }
+        let postamble = vec!["pop {ip, pc}".to_string()];
+        for instr in postamble {
+            writeln!(&assembly_file, "{}", instr).expect("write failure");
+        }
+
 
         let args = vec!["assemble-and-link-armv7.sh", &assembly_filename[..], &output_path];
         ::shell::run_shell_command("sh", &args)?;
@@ -107,39 +130,23 @@ impl ::Backend for ARMBackend {
     }
 }
 
-fn compile_expr(expr : &AstNode) -> CompilationUnit {
+fn compile_expr(basic_block : &mut BasicBlock, expr : &AstNode) {
     match expr {
         parser::AstNode::Integer(int) => {
-            let mut instrs = Vec::new();
-            instrs.push("ldr r0, =intfmt".to_string());
-            instrs.push(format!("mov r1, #{}", &int));
-            instrs.push("bl printf".to_string());
-            CompilationUnit::Integer(instrs)
+            basic_block.instructions.push(format!("mov r7, #{}", &int));
+            basic_block.instructions.push(format!("sub sp, sp, #4"));
+            basic_block.instructions.push(format!("str r7, [sp]"));
+            basic_block.value_refs.push(ValueRef {
+                offset: basic_block.base_offset
+            });
+            basic_block.base_offset += 4;
         },
         parser::AstNode::Terms(terms) => {
-            let mut units = Vec::new();
             for term in terms {
-                units.push(compile_expr(term));
+                compile_expr(basic_block, term);
             }
-            CompilationUnit::Terms(units)
         },
         _ => panic!("Not ready to compile expression: {:?}", expr)
-    }
-}
-
-fn compilation_units_to_instrs(instrs : &mut Vec<String>, units : Vec<CompilationUnit>) {
-    for unit in units {
-        match unit {
-            CompilerInstruction(instr) =>
-                instrs.push(instr),
-            CompilerInstructions(c_instrs)
-                | CompilationUnit::Integer(c_instrs) => {
-                instrs.extend(c_instrs)
-            },
-            CompilationUnit::Terms(units) => {
-                compilation_units_to_instrs(instrs, units)
-            }
-        }
     }
 }
 
