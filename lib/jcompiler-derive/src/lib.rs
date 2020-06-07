daf06635a4323498cbce0c35b8b51ef7c6603cd3
Matt Quinn <matt@mattjquinn.com>
2020-06-07 16:21:22 -0400

    Tests: code cleanup

--- lib/jcompiler-derive/src/lib.rs
+++ lib/jcompiler-derive/src/lib.rs
@@ -3,7 +3,7 @@ extern crate quote;
 extern crate syn;
 
 use proc_macro::{TokenStream};
-use proc_macro2::Span;
+use proc_macro2::{Span, Literal, Ident};
 use quote::quote;
 use std::iter::FromIterator;
 use syn::{Token, Lit};
@@ -12,15 +12,25 @@ use syn::punctuated::Punctuated;
 use std::collections::HashSet;
 
 struct TestInput {
-    known_test_failures: Punctuated<Lit, Token![,]>,
+    known_test_failures: HashSet<String>
 }
 
 impl Parse for TestInput {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let content;
         syn::bracketed!(content in input);
+        let inner_tokens : Punctuated<Lit, Token![,]> = content.parse_terminated(Lit::parse)?;
         Ok(TestInput{
-            known_test_failures: content.parse_terminated(Lit::parse)?
+            known_test_failures: inner_tokens.iter().filter_map(
+            |s| {
+                match s {
+                    Lit::Str(a) => Some(a.value()),
+                    _ => {
+                        println!("Warning: ignoring non-string literal in KTF list.");
+                        None
+                    }
+                }
+            }).collect()
         })
     }
 }
@@ -28,20 +38,7 @@ impl Parse for TestInput {
 #[proc_macro]
 pub fn generate_tests(input: TokenStream) -> TokenStream {
 
-    let test_input = syn::parse_macro_input!(input as TestInput);
-    let mut known_test_failures : HashSet<String> = test_input.known_test_failures.iter().filter_map(
-        |s| {
-            match s {
-                Lit::Str(a) => {
-                    Some(a.value())
-                },
-                _ => {
-                    println!("Warning: ignoring non-string literal in KTF list.");
-                    None
-                }
-            }
-        }
-    ).collect();
+    let mut test_input = syn::parse_macro_input!(input as TestInput);
 
     let mut entries: Vec<String> = std::fs::read_dir("jlang_programs").expect("dir")
         .map(|res| res.map(|e| e.path()))
@@ -50,9 +47,9 @@ pub fn generate_tests(input: TokenStream) -> TokenStream {
             let pathbuf = p.expect("pathbuf");
             let filename = pathbuf.file_name().expect("filename").to_str().expect("str");
             if !filename.ends_with(".ijs") { panic!("A test file in the test directory doesn't end in .ijs as it should.") }
-            if known_test_failures.contains(filename) {
+            if test_input.known_test_failures.contains(filename) {
                 println!("Ignoring known test failure: {}", filename);
-                known_test_failures.remove(filename);
+                test_input.known_test_failures.remove(filename);
                 return None
             }
             return Some(filename.to_string())
@@ -60,18 +57,16 @@ pub fn generate_tests(input: TokenStream) -> TokenStream {
         .collect();
     entries.sort();
 
-    if known_test_failures.len() > 0 {
-        panic!("One or more KTFs didn't match an actual test file: {:?}", known_test_failures)
+    if test_input.known_test_failures.len() > 0 {
+        panic!("One or more KTFs didn't match an actual test file: {:?}", test_input.known_test_failures)
     }
 
     let mut streams : Vec<TokenStream> = vec![];
-    let mut idx = 0;
-
     entries.iter().for_each(|test_filename| {
         let test_name = &test_filename[..&test_filename.len()-4];
 
-        let filename = proc_macro2::Literal::string(test_filename);
-        let methodname = proc_macro2::Ident::new(test_name, Span::call_site());
+        let filename = Literal::string(test_filename);
+        let methodname = Ident::new(test_name, Span::call_site());
         streams.push(
             (quote! {
                 #[test]
@@ -80,7 +75,6 @@ pub fn generate_tests(input: TokenStream) -> TokenStream {
                 }
             }).into()
         );
-        idx += 1;
     });
     TokenStream::from_iter(streams.into_iter())
 }
